ee41a250ed1408fa44b49c6fc7d196965fa7a3f8
Akash Das <das88768@gmail.com>
2022-10-16 21:08:40 +0530

    connect the host server with the home page

--- eBasket/eBasket/settings.py
+++ eBasket/eBasket/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'eBasket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['eBasket/shop/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- eBasket/eBasket/urls.py
+++ eBasket/eBasket/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Fetch all the urls from the main app 'shop'.
     path('shop/', include("shop.urls")),
+    path('', views.index),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
